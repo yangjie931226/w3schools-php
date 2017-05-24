0fdcadfd81a3f704bc381fe2f6ddeaea7ac13c98
yangjie931226 <13512788604@163.com>
2017-05-24 14:24:40 +0800

    Mysql Prepared Statements

---
+++ exercise31MySQLiObject-Oriented.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * 功能：        PHP Prepared Statements
+ * @author      杨杰 <yangjie@163.com>
+ * @version     v 1.0
+ */
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $firstname, $lastname, $email);
+
+$firstname = "John";
+$lastname = "Doe";
+$email = "john@example.com";
+$stmt->execute();
+
+$firstname = "Mary";
+$lastname = "Moe";
+$email = "mary@example.com";
+$stmt->execute();
+
+$firstname = "Julie";
+$lastname = "Dooley";
+$email = "julie@example.com";
+$stmt->execute();
+
+echo "New records created successfully";
+
+$stmt->close();
+$conn->close();
+
+?>
---
+++ exercise31PDO.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * 功能：        PHP Prepared Statements
+ * @author      杨杰 <yangjie@163.com>
+ * @version     v 1.0
+ */
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email)
+    VALUES (:firstname, :lastname, :email)");
+    $stmt->bindParam(':firstname', $firstname);
+    $stmt->bindParam(':lastname', $lastname);
+    $stmt->bindParam(':email', $email);
+
+    $firstname = "John";
+    $lastname = "Doe";
+    $email = "john@example.com";
+    $stmt->execute();
+
+    $firstname = "Mary";
+    $lastname = "Moe";
+    $email = "mary@example.com";
+    $stmt->execute();
+
+    $firstname = "Julie";
+    $lastname = "Dooley";
+    $email = "julie@example.com";
+    $stmt->execute();
+
+    echo "New records created successfully";
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+?>
