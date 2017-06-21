32e1282dd0bc130b7edd0820cbcca903bfabab72
yangjie931226 <13512788604@163.com>
2017-06-21 11:09:09 +0800

    48

---
+++ exercise48.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author 杨杰 <yangjie@163.com>
+ * @version 1.0
+ * @copyright 2014-2017 海量云图（北京）数据技术有限公司
+ */
+
+$myXMLData = "<?xml version='1.0' encoding='UTF-8'?>
+<note>
+<to>Tove</to>
+<from>Jani</from>
+<heading>Reminder</heading>
+<body>Don't forget me this weekend!</body>
+</note>";
+
+$xml = simplexml_load_string($myXMLData) or die("Error: Cannot create object");
+print_r($xml);
