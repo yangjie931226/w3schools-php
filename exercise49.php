8b5b84cdddf1739100c5e8448f92918530ffbf1d
yangjie931226 <13512788604@163.com>
2017-06-22 11:13:28 +0800

    49

---
+++ exercise49.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author 杨杰 <yangjie@163.com>
+ * @version 1.0
+ * @copyright 2014-2017 海量云图（北京）数据技术有限公司
+ */
+
+$xml = simplexml_load_file("note.xml") or die("Error: Cannot create object");
+echo $xml->to . "<br>";
+echo $xml->from . "<br>";
+echo $xml->heading . "<br>";
+echo $xml->body;
