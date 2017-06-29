57c655d5a1dd8fbe6142fbda3f76deeb7c4da2c3
yangjie931226 <13512788604@163.com>
2017-06-29 14:17:55 +0800

    54

---
+++ exercise54.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<?php
+/**
+ * @author 杨杰 <yangjie@163.com>
+ * @version 1.0
+ * @copyright 2014-2017 海量云图（北京）数据技术有限公司
+ */
+?>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
